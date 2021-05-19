7942a210ce778bc26722f96f2d5db8109e59fdfb
Nandan Kumar <nandankmrde@gmail.com>
2021-05-19 13:21:38 +0530

    Uploading_final_stucture

---
+++ DiagnosisTracker/App/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from App import views
+
+urlpatterns = [
+    # path("", views.hello_world),
+    path('', views.index, name="index"),
+    # path('about', views.about, name="about"),
+    # path('services', views.services, name="services"),
+    # path('contact', views.contact, name="contact"),
+    # path('signin', views.signin, name="signin"),
+    path('signup', views.signup, name="signup"),
+    path('welcome', views.welcome, name="welcome"),
+    path('main', views.main, name="main"),
+    path('logout', views.logout, name="log"),
+
+]
